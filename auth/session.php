caf8c10125294ff6c4bab79702580edc958801c5
Jeroen Hofstee <jhofstee@victronenergy.com>
2024-06-20 00:10:58 +0200

    store the session in a non-volatile location

--- auth/session.php
+++ auth/session.php
@@ -4,7 +4,9 @@ function venus_session_start($readonly = false)
 {
 	session_start([
 		'cookie_lifetime' => 365 * 24 * 60 * 60,
-		'read_and_close'  => $readonly
+		'read_and_close'  => $readonly,
+		'name' => 'VENUS_SESSION_ID',
+		'save_path' => '/data/var/venus-www-sessions'
 	]);
 }
 
