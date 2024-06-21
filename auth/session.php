a50d50e6668574e2fe00a122b809449f821ed989
Jeroen Hofstee <jhofstee@victronenergy.com>
2024-06-21 11:08:25 +0200

    /data/var/venus-www-sessions -> /data/var/lib/venus-www-sessions

--- auth/session.php
+++ auth/session.php
@@ -6,7 +6,7 @@ function venus_session_start($readonly = false)
 		'cookie_lifetime' => 365 * 24 * 60 * 60,
 		'read_and_close'  => $readonly,
 		'name' => 'VENUS_SESSION_ID',
-		'save_path' => '/data/var/venus-www-sessions'
+		'save_path' => '/data/var/lib/venus-www-sessions'
 	]);
 }
 
