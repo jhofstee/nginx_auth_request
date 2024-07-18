00a38f6b32ea48017a08fa49244a9ad2d833a425
Jeroen Hofstee <jhofstee@victronenergy.com>
2024-07-18 11:41:37 +0200

    logout.php: support passing a page to redirect to

--- auth/logout.php
+++ auth/logout.php
@@ -1,8 +1,10 @@
 <?php
 
+$page = isset($_GET["page"]) && $_GET["page"] != "" ? $_GET["page"] : "/";
 require('session.php');
 venus_session_start();
 session_destroy();
-header('Location: /');
+http_response_code(303); // See Other
+header('Location: ' . $page);
 ?>
 
